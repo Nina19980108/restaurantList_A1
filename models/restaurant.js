5b31ab9bc4616fad6aee887dcf44fda9c090be00
Nina19980108 <ninagirl8787@gmail.com>
2021-05-04 14:09:14 +0800

    feat: modify todo.js to restaurant.js

---
+++ models/restaurant.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Number,
+    require: true
+  },
+  description: {
+    type: String
+  },
+
+})
+
+module.exports = mongoose.model('Todo', todoSchema)
