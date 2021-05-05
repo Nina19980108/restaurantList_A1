de378f22c97d01e7d97ca3a999765e14aea44090
Nina19980108 <ninagirl8787@gmail.com>
2021-05-05 11:55:29 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -25,52 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-// app.get('/restaurant/new', (req, res) => {
-//   res.render('new')
-// })
-
-// app.post('/restaurant/new', (req, res) => {
-//   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-//   return restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.error(error))
-// })
-
-// app.get('/restaurant/:id', (req, res) => {
-//   const id = req.params.id
-//   return restaurant.findById(id)
-//     .lean()
-//     .then(restaurant => res.render('detail', { restaurant }))
-//     .catch(error => console.error(error))
-// })
-
-// app.get('/restaurant/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return restaurant.findById(id)
-//     .lean()
-//     .then(restaurant => res.render('edit', { restaurant }))
-//     .catch(error => console.error(error))
-// })
-
-// app.put('/restaurant/:id', (req, res) => {
-//   const id = req.params.id
-//   return restaurant.findById(id)
-//     .then(restaurant => {
-//       restaurant = Object.assign(restaurant, req.body)
-//       return restaurant.save()
-//     })
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.error(error))
-// })
-
-// app.delete('/restaurant/:id', (req, res) => {
-//   const id = req.params.id
-//   return restaurant.findById(id)
-//     .then(restaurant => restaurant.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.error(error))
-// })
-
 app.listen(port, () => {
   console.log(`Restaurant list is on http://localhost:${port}`)
 })
