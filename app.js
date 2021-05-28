94c8be883b97d0f2c039cdfdc2be242170ecd31c
Nina19980108 <ninagirl8787@gmail.com>
2021-05-28 13:59:55 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const port = 3000
-const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
+const port = 3000
+const app = express()
+
 const routes = require('./routes')
 
 require('./config/mongoose')
